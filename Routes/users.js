2539796d55071eb927555ff1b96e171e5bdd41cb
hassayouneM <mouna.hassayoune@gmail.com>
2022-04-30 05:13:09 +0100

    edit profile incomplete

--- Routes/users.js
+++ Routes/users.js
@@ -11,6 +11,7 @@ router.post("/register", userController.register);
 router.post("/findById",userController.get);
 router.post("/findByEmail",userController.getByEmail);
 router.post("/getPatients", userController.getPatients);
+router.post("/getAssistantName",userController.getAssistantName);
 router.post("/forgetPassword",userController.forgetPass);
 router.post("/reSendConfirmationEmail",userController.reSendConfirmationEmail);
 router.post("/loginWithSocial",userController.loginWithSocial);
